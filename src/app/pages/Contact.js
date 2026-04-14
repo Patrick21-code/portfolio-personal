947f0353cd7680d70b12995a57283c7bfd745303
patpat <tomaspatrickdanielle@gmail.com>
2026-04-15 07:59:06 +0800

    feat: add gmail functionality and github and linkedin links

--- src/app/pages/Contact.js
+++ src/app/pages/Contact.js
@@ -1,7 +1,18 @@
+"use client"
 
+import { toast } from "sonner"
 import { Button } from '@/components/ui/button'
 import { Gmail, GitHub, LinkedIn } from '@/components/icons/index'
+import { CheckCircle2Icon } from "lucide-react"
+
 export default function Contact () {
+
+    const handleEmailClick = () => {
+        navigator.clipboard.writeText("tomaspatrickdanielle@gmail.com")
+        toast.success("Email copied to clipboard"), {
+            icon: <CheckCircle2Icon />
+        }
+    }
 
     return (
         <div className="flex flex-col items-center justify-center">
@@ -9,10 +20,15 @@ export default function Contact () {
                 <p>Get In Touch</p>
                 <p>Have questions, feedback, or just want to say hello? I’d be happy to hear from you and will do my best to respond thoughtfully.</p>
             </div>
+
             <div className="flex gap-2">
-                <Button className="border-2 rounded-xl px-4"><Gmail /> Gmail</Button>
-                <Button className="border-2 rounded-xl px-4"><GitHub /> GitHub</Button>
-                <Button className="border-2 rounded-xl px-4"><LinkedIn /> LinkedIn</Button>
+                <Button className="border-2 rounded-xl px-4" onClick={handleEmailClick}><Gmail /> Gmail</Button>
+                <a href="https://github.com/Patrick21-code" target="_blank" >
+                    <Button className="border-2 rounded-xl px-4"><GitHub /> GitHub</Button>
+                </a>
+                <a href="https://www.linkedin.com/in/patrick-danielle-tomas-441395353?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" >
+                    <Button className="border-2 rounded-xl px-4"><LinkedIn /> LinkedIn</Button>
+                </a>
             </div>
         </div>
     )
