5378824d9be1cf69c43eda23f930025f3046ab6b
patpat <tomaspatrickdanielle@gmail.com>
2026-04-14 19:34:53 +0800

    feat: add about page

---
+++ src/app/pages/About.js
@@ -0,0 +1,41 @@
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { HTML5, CSS, JavaScript, TailwindCSS, React, Nextjs, MySQL, Git} from '@/components/icons'
+
+export default function About () {
+
+    return (
+        <div className="flex justify-between">
+            <div> {/*about*/}
+                <p>
+                    Hi, I’m Patrick. I focus on frontend development and enjoy working with React to build interfaces that feel clear
+                    and easy to use. I’m still learning and growing as a developer, but I take pride in creating simple, practical
+                    solutions that make ideas come to life.
+                </p>
+            </div>
+            <div> {/*tech skills*/}
+                <Card>
+                    <CardContent>
+                        <HTML5 className="size-10" />
+                        <CSS className="size-10" />
+                        <JavaScript className="size-10" />
+                        <TailwindCSS className="size-10" />
+                        <React className="size-10" />
+                        <Nextjs className="size-10" />
+                        <MySQL className="size-10" />
+                        <Git className="size-10" />
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    )
+}
