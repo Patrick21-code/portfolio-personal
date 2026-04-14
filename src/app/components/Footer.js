d390a44c12d677ac63f448909b3d1a82fc382790
patpat <tomaspatrickdanielle@gmail.com>
2026-04-14 22:54:29 +0800

    feat: add footer page

---
+++ src/app/components/Footer.js
@@ -0,0 +1,9 @@
+
+export default function Footer () {
+
+    return (
+        <div className="flex justify-center"> 
+            <p>&copy; 2026 Patrick Danielle Tomas. All rights reserved</p>
+        </div>
+    )
+}
