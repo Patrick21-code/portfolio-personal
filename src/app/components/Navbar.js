ce529dd843aa974413315f63f26282d8dd80e3a0
patpat <tomaspatrickdanielle@gmail.com>
2026-04-14 17:57:42 +0800

    feat: add navbar links

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,15 @@
+
+export default function Navbar () {
+
+    return (
+        <div className="flex justify-between">
+            <h1>Patrick.dev</h1>
+            <nav>
+                <a>Home</a>
+                <a>About</a>
+                <a>Projects</a>
+                <a>Contact</a>
+            </nav>
+        </div>
+    )
+}
